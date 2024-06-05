1d35762e36ae0da4b4d17ceb1febdd85b3e4ee22
fedemortimer <fmortimer@fi.uba.ar>
2024-06-05 20:09:27 -0300

    #19 Uploaded Question Class

---
+++ src/main/java/tp2/clases/Question.java
@@ -0,0 +1,41 @@
+package tp2.clases;
+import java.util.HashMap;
+import java.util.List;
+
+public class Question {
+    private String theme;
+    private List<Answer> options;
+    private String content;
+    private Type type;
+    private int numberOfCorrectAnswers;
+
+
+    public Question(String content, Type type, List<Answer> answers, String theme) {
+        this.options = answers;
+        this.theme = theme;
+        this.content = content;
+        this.type = type;
+
+    }
+
+    public int getNumberOfCorrectAnswers() {
+        for (Answer answer : options) {
+            if (answer.getCorrection() instanceof Correct){
+                numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+            }
+        }
+        return numberOfCorrectAnswers;
+    }
+
+    public void assignScore(HashMap<Player, Answer> playersAnswers) {
+        type.assignScore(playersAnswers, numberOfCorrectAnswers);
+    }
+
+    public void choiceOption(){
+
+    }
+
+
+
+
+}
