702f1a1195f6d168c6af642c7d168b6018a59c52
fedemortimer <fmortimer@fi.uba.ar>
2024-06-06 01:49:49 -0300

    #37 Changed class method

--- src/main/java/tp2/clases/MultipleChoice.java
+++ src/main/java/tp2/clases/MultipleChoice.java
@@ -6,19 +6,32 @@ public class MultipleChoice extends Type {
 
     @Override
     public void assignScore(HashMap<Player, ArrayList<Answer>> chosenAnswers, int numberOfCorrectAnswers) {
-        int numberOfCorrectAnswersOfPlayer = 0;
 
         for (Player player : chosenAnswers.keySet()) {
             ArrayList<Answer> answers = chosenAnswers.get(player);
 
+            boolean hasIncorrectAnswer = false;
+
             for (Answer answer : answers) {
-                if (answer.getCorrection() instanceof Correct) {
-                    numberOfCorrectAnswersOfPlayer++;
+                if (answer.getCorrection() instanceof Incorrect) {
+                    hasIncorrectAnswer = true;
+                    break;
+                }
+            }
+
+            if (!hasIncorrectAnswer) {
+                int numberOfCorrectAnswersOfPlayer = 0;
+                for (Answer answer : answers) {
+                    if (answer.getCorrection() instanceof Correct) {
+                        numberOfCorrectAnswersOfPlayer++;
+                    }
                 }
                 if (numberOfCorrectAnswersOfPlayer == numberOfCorrectAnswers) {
                     player.setScore(player.getScore() + 1);
-                } 
-            }   
+                }
+            } else {
+                player.setScore(0);
+            }
         }
     }
 }
