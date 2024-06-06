e200e7604dfe9fb3987be16d18e08b19c521675e
Daniel Mamani <danymamani1002@gmail.com>
2024-06-06 14:04:49 -0300

    #49 modified PlayerTest and ScoreTest class

--- src/test/java/tp2/clases/PlayerTest.java
+++ src/test/java/tp2/clases/PlayerTest.java
@@ -1,6 +1,8 @@
 package tp2.clases;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -10,29 +12,49 @@ import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 
 public class PlayerTest {
-    
     private Player player;
-    @Mock private Question question;
-    @Mock private ArrayList<Answer> answers;
-    @Mock private Correction correction;
-    @Mock private Correct correct;
-    @Mock private Incorrect incorrect;
+    @Mock private Question questionMock;
+    @Mock private Answer answerMock;
+    @Mock private Correct correctMock;
+    @Mock private Incorrect incorrectMock;
+
+    private AutoCloseable closeable;
+
+    private ArrayList<Answer> chosenAnswers;
+
+    //mocks are initialized
+    @BeforeEach
+    public void beforeEach(){
+        closeable = MockitoAnnotations.openMocks(this);
+        Mockito.reset(questionMock, answerMock, correctMock, incorrectMock);
+
+        chosenAnswers = new ArrayList<Answer>();
+        chosenAnswers.add(answerMock);
+
+        when(questionMock.choiceOption("")).thenReturn(chosenAnswers);
+        when(chosenAnswers.get(0).getCorrection()).thenReturn(correctMock);
+        when(chosenAnswers.get(0).getScore()).thenReturn(1);
+    }
+
+    @AfterEach
+    public void releaseMocks() throws Exception {
+        closeable.close();
+    }
     
     @Test
     public void test01AnsweringAQuestionCorrectlyIncreasesTheScore(){
         //Arrange
-        when(player.answer(question, "")).thenReturn(answers);
-        when(answers.get(0).getCorrection()).thenReturn(correct);
-        when(answers.get(0).getScore()).thenReturn(1);
+        when(chosenAnswers.get(0).getCorrection()).thenReturn(correctMock);
+        when(correctMock.assignScore(1)).thenReturn(1);
 
         int expectedScore = 1;
         player = new Player("name", 0);
 
         //Act
-        ArrayList<Answer> answers = player.answer(question, "");
+        ArrayList<Answer> answers = player.answer(questionMock, "");
         player.assignScore(answers.get(0).getCorrection(), answers.get(0).getScore());
         int scoreObtained = player.getScore();
-        
+
         //Assert
         assertEquals(expectedScore, scoreObtained);
         
@@ -41,15 +63,14 @@ public class PlayerTest {
     @Test
     public void test02AnsweringAQuestionIncorrectlyDecreasesTheScore(){
         //Arrange
-        when(player.answer(question)).thenReturn(answers);
-        when(answers.get(0).getCorrection()).thenReturn(incorrect);
-        when(answers.get(0).getScore()).thenReturn(1);
+        when(chosenAnswers.get(0).getCorrection()).thenReturn(incorrectMock);
+        when(incorrectMock.assignScore(1)).thenReturn(-1);
 
         int expectedScore = 0;
         player = new Player("name", 1);
 
         //Act
-        ArrayList<Answer> answers = player.answer(question, "");
+        ArrayList<Answer> answers = player.answer(questionMock, "");
         player.assignScore(answers.get(0).getCorrection(), answers.get(0).getScore());
         int scoreObtained = player.getScore();
 
--- src/test/java/tp2/clases/ScoreTest.java
+++ src/test/java/tp2/clases/ScoreTest.java
@@ -1,48 +1,65 @@
 package tp2.clases;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+
+import java.util.ArrayList;
 
 public class ScoreTest {
     private Score score;
-    @Mock private Correction correction;
-    @Mock private Correct correct;
-    @Mock private Incorrect incorrect;
+    @Mock private Correct correctMock;
+    @Mock private Incorrect incorrectMock;
+
+    private AutoCloseable closeable;
+
+    //mocks are initialized
+    @BeforeEach
+    public void beforeEach(){
+        closeable = MockitoAnnotations.openMocks(this);
+        Mockito.reset(correctMock, incorrectMock);
+    }
+
+    @AfterEach
+    public void releaseMocks() throws Exception {
+        closeable.close();
+    }
 
     @Test
     public void test01ACorrectCorrectionIncreasesTheScore(){
         //Arrange
+        when(correctMock.assignScore(1)).thenReturn(1);
 
         int expectedScore = 10;
         score = new Score(9);
 
         //Act
-
-        score.assignScore(correct, 1);
+        score.assignScore(correctMock, 1);
         int scoreObtained = score.getScore();
 
         //Assert
-
         assertEquals(expectedScore, scoreObtained);
     }
 
     @Test
     public void test02AIncorrectCorrectionDecreasesTheScore(){
         //Arrange
+        when(incorrectMock.assignScore(1)).thenReturn(-1);
 
         int expectedScore = 0;
         score = new Score(1);
 
         //Act
-
-        score.assignScore(correct, 1);
+        score.assignScore(incorrectMock, 1);
         int scoreObtained = score.getScore();
 
         //Assert
-
         assertEquals(expectedScore, scoreObtained);
     }
 }
