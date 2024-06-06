abef088a29cbf0f81d14c19a56dbc997ea4e0ad8
agPelliciari <apelliciari@fi.uba.ar>
2024-06-06 00:13:11 -0300

    #32 Added test names

--- src/test/java/tp2/clases/CasesOfUseTest.java
+++ src/test/java/tp2/clases/CasesOfUseTest.java
@@ -1,4 +1,46 @@
 package tp2.clases;
 
+import org.junit.jupiter.api.Test;
+
 public class CasesOfUseTest {
+
+    @Test
+    public void test01ATrueFalseQuestionReceivesAListOfAnswersAndAssignsPointsToThoseWhoAnsweredCorrectly() {
+        
+    }
+
+    @Test
+    public void test02ATrueFalseQuestionReceivesAListOfAnswersAndAssignsPointsToThoseWhoAnsweredIncorrectly() {
+        
+    }
+
+    @Test
+    public void test03AMultipleChoiceClassicQuestionReceivesAListOfAnswersOfAPlayerThatAnsweredCorrectlyAndAssignsTheScore() {
+        
+    }
+
+    @Test
+    public void test04AMultipleChoiceClassicQuestionReceivesAListOfAnswersOfAPlayerThatAnsweredIncorrectlyAndAssignsTheScore() {
+        
+    }
+
+    @Test
+    public void test05ATrueFalseQuestionWithPenaltyReceivesAListOfAnswersAndAssignsPointsToThoseWhoAnsweredCorrectly() {
+        
+    }
+
+    @Test
+    public void test06ATrueFalseQuestionWithPenaltyReceivesAListOfAnswersAndAssignsPointsToThoseWhoAnsweredIncorrectly() {
+        
+    }
+
+    @Test
+    public void test07AMultipleChoiceQuestionWithPenaltyReceivesAListOfAnswersOfAPlayerThatAnsweredCorrectlyAndAssignsTheScore() {
+        
+    }
+
+    @Test
+    public void test08AMultipleChoiceQuestionWithPenaltyReceivesAListOfAnswersOfAPlayerThatAnsweredIncorrectlyAndAssignsTheScore() {
+        
+    }
 }
