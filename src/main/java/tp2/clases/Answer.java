e179ba23b0b180f19e768879a9acb3af8456a860
Daniel Mamani <danymamani1002@gmail.com>
2024-06-05 21:19:58 -0300

    #21 modified Question and Answer class

--- src/main/java/tp2/clases/Answer.java
+++ src/main/java/tp2/clases/Answer.java
@@ -4,11 +4,13 @@ public class Answer {
     public String content;
     public Correction correction;
     public int score;
+    private char id;
 
-    public Answer(String content, String correction, int score) {
+    public Answer(String content, String correction, int score, char id) {
         this.content = content;
         this.correction = Correction.assignCorrection(correction);
         this.score = score;
+        this.id = id;
     }
 
 //    public void assignScore(Player player) {
@@ -26,6 +28,12 @@ public class Answer {
 
     public int getScore() {
         return score;
+    }
+
+    public char getId() { return id;}
+
+    public boolean equals(char searchedId){
+        return getId() == searchedId;
     }
 }
 
--- src/main/java/tp2/clases/Question.java
+++ src/main/java/tp2/clases/Question.java
@@ -1,6 +1,8 @@
 package tp2.clases;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import tp2.clases.Player;
 
 public class Question {
     private String theme;
@@ -31,8 +33,14 @@ public class Question {
         type.assignScore(playersAnswers, numberOfCorrectAnswers);
     }
 
-    public void choiceOption(){
+    public ArrayList<Answer> choiceOption(String chosenOptions){
 
+        ArrayList<Answer> chosenAnswers = new ArrayList<Answer>();
+        for(char chosenOption: chosenOptions.toCharArray()){
+            chosenAnswers.add((Answer) options.stream().map(option -> option.equals(chosenOption)));
+        }
+
+        return chosenAnswers;
     }
 
 
