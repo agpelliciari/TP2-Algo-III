a84b052eb5da23e532765f5573a184d2c9141383
pedroaguilar97 <agui.pedro2013@gmail.com>
2024-06-11 14:50:31 -0300

    Update Option.java

--- src/main/java/tp2/clases/Option.java
+++ src/main/java/tp2/clases/Option.java
@@ -1,1 +1,25 @@
-//option
+package tp2.clases;
+
+public class Option {
+    private int number;
+    private String text;
+    private Correction correction;
+
+    public Option(int number, String text, Correction correction) {
+        this.number = number;
+        this.text = text;
+        this.correction = correction;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Correction getCorrection() {
+        return correction;
+    }
+}
