63eca882e8a9a75edaa992daa4eba596d25443b5
pedroaguilar97 <agui.pedro2013@gmail.com>
2024-06-13 09:08:04 -0300

    #75

--- src/main/java/tp2/clases/Main.java
+++ src/main/java/tp2/clases/Main.java
@@ -1,58 +1,65 @@
 package tp2.clases;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.Arrays;
-import java.io.FileReader;
-import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        try (Reader reader = new FileReader("src/main/resources/preguntas.json")) {
-            TypeToken<ArrayList<Question_>> questionListType = new TypeToken<ArrayList<Question_>>() {};
-            ArrayList<Question_> questions = gson.fromJson(reader, questionListType.getType());
-
-            for (Question_ question : questions) {
-                String[] options = {
-                        question.getOption1(),
-                        question.getOption2(),
-                        question.getOption3(),
-                        question.getOption4(),
-                        question.getOption5(),
-                        question.getOption6()
-                };
-                options = Arrays.stream(options).filter(option -> option != null && !option.isEmpty()).toArray(String[]::new);
-                question.setOptions(options);
-
-                if (question.getGroupAText() != null && question.getGroupBText() != null) {
-                    String[] groups = {
-                            question.getGroupAText(),
-                            question.getGroupBText()
-                    };
-                    question.setGroups(groups);
+        JsonParser jsonParser = new JsonParser();
+        ArrayList<Question> questions = jsonParser.questionsParser("src/main/resources/preguntas.json");
+        for (Question question : questions) {
+            System.out.println("ID: " + question.getId());
+            System.out.println("Tema: " + question.getContent().getTheme());
+            System.out.println("Pregunta: " + question.getContent().getPrompt());
+            System.out.print("Respuesta: ");
+            if (question instanceof OrderedChoice) {
+                String orderedResponse = Arrays.stream(((OrderedChoice) question).getCorrectOrder())
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(","));
+                System.out.print(orderedResponse);
+            } else if (question instanceof GroupChoice) {
+                StringBuilder answer = new StringBuilder();
+                boolean firstGroup = true;
+                for (Group group : ((GroupChoice) question).getGroups()) {
+                    if (!firstGroup) {
+                        answer.append("; ");
+                    }
+                    answer.append(group.getLetter()).append(": ");
+                    boolean firstChoice = true;
+                    for (Choice choice : group.getChoices()) {
+                        if (!firstChoice) {
+                            answer.append(",");
+                        }
+                        answer.append(choice.getId());
+                        firstChoice = false;
+                    }
+                    firstGroup = false;
+                }
+                System.out.print(answer.toString());
+            } else {
+                boolean first = true;
+                for (Choice choice : question.getChoices()) {
+                    if (choice.getCorrection().isCorrect()) {
+                        if (!first) {
+                            System.out.print(",");
+                        }
+                        System.out.print(choice.getId());
+                        first = false;
+                    }
                 }
             }
-
-            for (Question_ question : questions) {
-                System.out.println("ID: " + question.getID());
-                System.out.println("Tema: " + question.getTheme());
-                System.out.println("Tipo: " + question.getType());
-                System.out.println("Question_: " + question.getQuestion());
-                System.out.println("Respuesta: " + question.getAnswer());
-                for (Option option : question.getOptions())
-                    System.out.println("Opcion " + option.getNumber() + ": " + option.getText());
-                for (Group group : question.getGroups())
-                    System.out.println("Grupo " + group.getLetter() + ": " + group.getText());
-                System.out.println("Texto respuesta: " + question.getAnswerText());
-                System.out.println("Respuesta del usuario: 1,2");
-                System.out.println(question.checkAllAnswersAreCorrect("1,2"));
-                System.out.println();
+            System.out.println();
+            for (Choice choice : question.getChoices()) {
+                System.out.println("Opcion " + choice.getId() + ": " + choice.getContent());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (question instanceof GroupChoice) {
+                for (Group group : ((GroupChoice) question).getGroups()) {
+                    System.out.println("Grupo " + group.getLetter() + ": " + group.getText());
+                }
+            }
+            System.out.println("Texto Respuesta: " + question.getContent().getAnswerText());
+            System.out.println();
         }
     }
 }
