de32489dd352abf495d326ae46bb8723ddcba1c1
Daniel Mamani <danymamani1002@gmail.com>
2024-06-18 00:54:57 -0300

    #82 a basic example is made to test the correct installation of dependencies for the graphical interface

--- src/main/java/tp2/clases/Main.java
+++ src/main/java/tp2/clases/Main.java
@@ -7,9 +7,39 @@ import java.util.Arrays;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+import javax.swing.*;
+
+import javafx.application.Application;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("My first window");
+
+        StackPane layout = new StackPane();
+        Button button = new Button();
+        button.setText("button text");
+        layout.getChildren().add(button);
+        Scene scene = new Scene(new StackPane(layout), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
     public static void main(String[] args) {
+
+        //to run javaFx basic example
+        ///
+        launch(args);
+        ////
+
         Gson gson = new Gson();
         try (Reader reader = new FileReader("src/main/resources/preguntas.json")) {
             TypeToken<ArrayList<Question_>> questionListType = new TypeToken<ArrayList<Question_>>() {};
