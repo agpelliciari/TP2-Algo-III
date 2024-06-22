e4fd616c9e2d2465f8727b6cc53ee2a5ef1b0d86
agpelliciari <apelliciari@fi.uba.ar>
2024-06-22 13:49:11 -0300

    #56 Modified 'ChoiceFactory' and added test in 'ChoiceFactoryTest'

--- src/main/java/tp2/clases/ChoicesFactory.java
+++ src/main/java/tp2/clases/ChoicesFactory.java
@@ -1,28 +1,33 @@
 package tp2.clases;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 class ChoicesFactory {
 
     public static ArrayList<Choice> createAnswers(String chosenAnswers, Question question) {
         ArrayList<Choice> choices = new ArrayList<>();
-
         String[] chosenNumbers = chosenAnswers.split(",");
 
-        for (String chosenNumber : chosenNumbers) {
-            int choiceId = Integer.parseInt(chosenNumber.trim());
+        HashMap<Integer, Choice> questionChoicesMap = new HashMap<>();
+        for (Choice choice : question.getChoices()) {
+            questionChoicesMap.put(choice.getId(), choice);
+        }
 
-            for (Choice choice : question.getChoices()) {
-                if (choice.getId() == choiceId) {
-                    if (choice.getCorrection().isCorrect()) {
-                        choices.add(new Choice(choice.getContent(), "Correcta", choice.getId()));
-                    } else {
-                        choices.add(new Choice(choice.getContent(), "Incorrecta", choice.getId()));
-                    }
-                    break;
-                }
+        for (String chosenNumber : chosenNumbers) {
+            try {
+                int choiceId = Integer.parseInt(chosenNumber.trim());
+                Choice questionChoice = questionChoicesMap.get(choiceId);
+
+                String correctionType = questionChoice.getCorrection().isCorrect() ? "Correcta" : "Incorrecta";
+                choices.add(new Choice(questionChoice.getContent(), correctionType, questionChoice.getId()));
+
+            } catch (NumberFormatException | NullPointerException error) {
+                // Handling errors in case of an out-of-bounds answer the answer will automatically be incorrect
+                choices.add(new Choice("", "Incorrecta", -1));
             }
         }
+
         return choices;
     }
 }
--- src/test/java/tp2/clases/ChoicesFactoryTest.java
+++ src/test/java/tp2/clases/ChoicesFactoryTest.java
@@ -52,4 +52,54 @@ public class ChoicesFactoryTest {
         //Assert
         assertFalse(answer.getCorrection().isCorrect());
     }
+
+    @Test
+    public void test03FactoryCreatesMultipleInstances() {
+        //Arrange
+
+        ArrayList<Choice> choices = new ArrayList<>();
+
+        choices.add(new Choice("a", "correcta", 1));
+        choices.add(new Choice("b", "Incorrecta", 2));
+        choices.add(new Choice("c", "correcta", 3));
+        choices.add(new Choice("d", "Incorrecta", 4));
+
+        MultipleChoice question = new MultipleChoice(1, content, new ClassicMode(), choices);
+
+        //Act
+
+        ArrayList<Choice> answers = ChoicesFactory.createAnswers("1,2,3", question);
+
+        Choice answerOne = answers.get(0);
+        Choice answerTwo = answers.get(1);
+        Choice answerThree = answers.get(2);
+
+        //Assert
+
+        assertTrue(answerOne.getCorrection().isCorrect());
+        assertFalse(answerTwo.getCorrection().isCorrect());
+        assertTrue(answerThree.getCorrection().isCorrect());
+    }
+
+    @Test
+    public void test04FactoryHandlesAnOutOfBoundsAnswer() {
+        //Arrange
+
+        ArrayList<Choice> choices = new ArrayList<>();
+
+        choices.add(new Choice("Verdadero", "correcta", 1));
+        choices.add(new Choice("Falso", "Incorrecta", 2));
+
+        TrueOrFalse question = new TrueOrFalse(1, content, new ClassicMode(), choices);
+
+        //Act
+
+        ArrayList<Choice> answers = ChoicesFactory.createAnswers("1234432344", question);
+
+        Choice answer = answers.get(0);
+
+        //Assert
+
+        assertFalse(answer.getCorrection().isCorrect());
+    }
 }
