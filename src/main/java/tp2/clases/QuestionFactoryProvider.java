190af6d211d181502a75fda474781f08abb57f41
agpelliciari <apelliciari@fi.uba.ar>
2024-06-25 00:20:02 -0300

    #96 Added 'QuestionFactoryProvider' test and class

---
+++ src/main/java/tp2/clases/QuestionFactoryProvider.java
@@ -0,0 +1,17 @@
+package tp2.clases;
+
+public class QuestionFactoryProvider {
+
+    public static QuestionFactory getFactory(String type) {
+        return switch (type) {
+            case "Verdadero Falso Simple", "Verdadero Falso" -> new TrueOrFalseFactory(new ClassicMode());
+            case "Verdadero Falso Penalidad" -> new TrueOrFalseFactory(new PenaltyMode());
+            case "Multiple Choice Simple" -> new MultipleChoiceFactory(new ClassicMode());
+            case "Multiple Choice Puntaje Parcial" -> new MultipleChoiceFactory(new PartialMode());
+            case "Multiple Choice Penalidad" -> new MultipleChoiceFactory(new PenaltyMode());
+            case "Ordered choice", "Ordered Choice" -> new OrderedChoiceFactory();
+            case "Group Choice" -> new GroupChoiceFactory();
+            default -> throw new IllegalStateException();
+        };
+    }
+}
