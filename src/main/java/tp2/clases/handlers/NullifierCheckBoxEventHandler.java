f1695ae3ed339d3fe282027263f2bc1b1ebc745c
Daniel Mamani <danymamani1002@gmail.com>
2024-06-25 00:04:25 -0300

    #97 created 'NullifierCheckBoxEventHandler' class

---
+++ src/main/java/tp2/clases/handlers/NullifierCheckBoxEventHandler.java
@@ -0,0 +1,33 @@
+package tp2.clases.handlers;
+
+import javafx.scene.control.Alert;
+import tp2.clases.Game;
+import tp2.clases.Player;
+import tp2.clases.exceptions.UsedPowerException;
+
+public class NullifierCheckBoxEventHandler {
+    private Game game;
+
+    public NullifierCheckBoxEventHandler(Game game) {
+        this.game = game;
+    }
+
+    public void selectNullifier(Player player, boolean selectedNullifier) {
+        if (selectedNullifier) {
+            game.activateNullifier();
+            try {
+                player.useNullifier();
+            } catch (UsedPowerException e) {
+                showErrorDialog(e.getMessage());
+            }
+        }
+    }
+
+    private void showErrorDialog(String errorMessage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(errorMessage);
+        alert.showAndWait();
+    }
+}
