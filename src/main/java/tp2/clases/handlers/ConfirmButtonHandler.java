5e3363766f09041cec1a345fe3949a928f2ff388
agpelliciari <apelliciari@fi.uba.ar>
2024-06-26 17:22:12 -0300

    #91 Added input screen response to 'ConfirmButtonHandler'

--- src/main/java/tp2/clases/handlers/ConfirmButtonHandler.java
+++ src/main/java/tp2/clases/handlers/ConfirmButtonHandler.java
@@ -10,15 +10,21 @@ public class ConfirmButtonHandler implements EventHandler<ActionEvent> {
 
     private PlayersInputScreen playersInputScreen;
     private Consumer<Integer> numberOfPlayersConsumer;
+    private Consumer<Integer> numberOfQuestionsConsumer;
+    private Consumer<Integer> numberOfPointsConsumer;
 
-    public ConfirmButtonHandler(PlayersInputScreen playersInputScreen, Consumer<Integer> numberOfPlayersConsumer) {
+    public ConfirmButtonHandler(PlayersInputScreen playersInputScreen, Consumer<Integer> numberOfPlayersConsumer, Consumer<Integer> numberOfQuestionsConsumer, Consumer<Integer> numberOfPointsConsumer) {
         this.playersInputScreen = playersInputScreen;
         this.numberOfPlayersConsumer = numberOfPlayersConsumer;
+        this.numberOfQuestionsConsumer = numberOfQuestionsConsumer;
+        this.numberOfPointsConsumer = numberOfPointsConsumer;
     }
 
     @Override
     public void handle(ActionEvent event) {
-        playersInputScreen.confirmNumberOfPlayers(numberOfPlayersConsumer);
+        playersInputScreen.confirmNumber(playersInputScreen.getNumberOfPlayersTextField(), numberOfPlayersConsumer, 1);
+        playersInputScreen.confirmNumber(playersInputScreen.getNumberOfQuestionsTextField(), numberOfQuestionsConsumer, 1);
+        playersInputScreen.confirmNumber(playersInputScreen.getNumberOfPointsTextField(), numberOfPointsConsumer, 1);
     }
 }
 
