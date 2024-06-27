be349e529e5a1245ce8745ab1603e42354cadb49
pedroaguilar97 <agui.pedro2013@gmail.com>
2024-06-27 09:10:41 -0300

    #106

--- src/test/java/tp2/clases/GroupChoiceTest.java
+++ src/test/java/tp2/clases/GroupChoiceTest.java
@@ -2,7 +2,7 @@ package tp2.clases;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GroupChoiceTest {
 
@@ -10,9 +10,10 @@ class GroupChoiceTest {
     public void test01PlayerCorrectlyPlacesChoicesInGroups() {
         Player player = new Player("Player", 0);
 
+        // Definición de grupos y opciones
         ArrayList<Group> groups = new ArrayList<>();
-        groups.add(new Group('A',"", new int[]{1,2,5}));
-        groups.add(new Group('B',"", new int[]{3,4,6}));
+        groups.add(new Group('A', "", new int[]{1, 2, 5}));
+        groups.add(new Group('B', "", new int[]{3, 4, 6}));
 
         ArrayList<Choice> choices = new ArrayList<>();
         choices.add(new Choice("", "correcta", 1));
@@ -28,8 +29,13 @@ class GroupChoiceTest {
         ArrayList<Choice> chosenAnswersGroupA = player.setAnswers(groupChoice, "1,2,5");
         ArrayList<Choice> chosenAnswersGroupB = player.setAnswers(groupChoice, "3,4,6");
 
-        groupChoice.getGroups().get(0).assignScore(player, chosenAnswersGroupA);
-        groupChoice.getGroups().get(1).assignScore(player, chosenAnswersGroupB);
+        ArrayList<ArrayList<Choice>> groupsChosenAnswers = new ArrayList<>();
+        groupsChosenAnswers.add(chosenAnswersGroupA);
+        groupsChosenAnswers.add(chosenAnswersGroupB);
+
+        int score = groupChoice.calculateTotalScore(groupsChosenAnswers);
+
+        player.addToScore(score);
 
         assertEquals(2, player.getScore());
     }
@@ -38,9 +44,10 @@ class GroupChoiceTest {
     public void test02PlayerIncorrectlyPlacesChoicesInGroups() {
         Player player = new Player("Player", 0);
 
+        // Definición de grupos y opciones
         ArrayList<Group> groups = new ArrayList<>();
-        groups.add(new Group('A',"", new int[]{1,2,5}));
-        groups.add(new Group('B',"", new int[]{3,4,6}));
+        groups.add(new Group('A', "", new int[]{1, 2, 5}));
+        groups.add(new Group('B', "", new int[]{3, 4, 6}));
 
         ArrayList<Choice> choices = new ArrayList<>();
         choices.add(new Choice("", "correcta", 1));
@@ -56,8 +63,13 @@ class GroupChoiceTest {
         ArrayList<Choice> chosenAnswersGroupA = player.setAnswers(groupChoice, "1,3,6");
         ArrayList<Choice> chosenAnswersGroupB = player.setAnswers(groupChoice, "2,4,5");
 
-        groupChoice.getGroups().get(0).assignScore(player, chosenAnswersGroupA);
-        groupChoice.getGroups().get(1).assignScore(player, chosenAnswersGroupB);
+        ArrayList<ArrayList<Choice>> groupsChosenAnswers = new ArrayList<>();
+        groupsChosenAnswers.add(chosenAnswersGroupA);
+        groupsChosenAnswers.add(chosenAnswersGroupB);
+
+        int score = groupChoice.calculateTotalScore(groupsChosenAnswers);
+
+        player.addToScore(score);
 
         assertEquals(0, player.getScore());
     }
