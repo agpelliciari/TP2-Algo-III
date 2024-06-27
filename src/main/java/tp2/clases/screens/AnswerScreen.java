b6458ab74614832b824e502fc74c7c89eb18adfe
Daniel Mamani <danymamani1002@gmail.com>
2024-06-26 21:29:14 -0300

    #102 added PowerUsed on Screen an modify AnswerScreen class

--- src/main/java/tp2/clases/screens/AnswerScreen.java
+++ src/main/java/tp2/clases/screens/AnswerScreen.java
@@ -7,12 +7,15 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import tp2.clases.Player;
 import tp2.clases.handlers.ActionHandler;
 import tp2.clases.handlers.ContinueButtonEventHandler;
 
+import java.util.ArrayList;
+
 public class AnswerScreen extends VBox {
 
-    public AnswerScreen(ActionHandler continueHandler, String answerText) {
+    public AnswerScreen(ActionHandler continueHandler, String answerText, ArrayList<Player> players) {
         this.setAlignment(Pos.CENTER);
         this.setSpacing(20);
         this.setPadding(new Insets(20));
@@ -24,6 +27,9 @@ public class AnswerScreen extends VBox {
         Label answer = new Label(answerText);
         answer.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         this.getChildren().add(answer);
+
+        PowersUsedScreen powersUsedScreen = new PowersUsedScreen(players);
+        this.getChildren().add(powersUsedScreen);
 
         Button continueButton = new Button("Continuar");
         continueButton.setFont(Font.font("Arial", FontWeight.BOLD, 16));
