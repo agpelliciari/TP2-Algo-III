b4b3ab31849ef78b266f79d93c393fbf5fdd7a87
pedroaguilar97 <agui.pedro2013@gmail.com>
2024-07-03 11:34:24 -0300

    #126

--- src/main/java/tp2/clases/questions/types/GroupChoice.java
+++ src/main/java/tp2/clases/questions/types/GroupChoice.java
@@ -1,6 +1,5 @@
 package tp2.clases.questions.types;
 
-import tp2.clases.*;
 import tp2.clases.player.Player;
 import tp2.clases.questions.choice.Choice;
 import tp2.clases.questions.Content;
@@ -10,10 +9,56 @@ import java.util.ArrayList;
 
 public class GroupChoice extends Question {
 
+    public static class Group {
+        private final char letter;
+        private final String text;
+        private final ArrayList<Choice> choices = new ArrayList<>();
+
+        public Group(char letter, String text, int[] intChoices) {
+            this.letter = letter;
+            this.text = text;
+            for (int intChoice : intChoices) {
+                choices.add(new Choice("", "correcta", intChoice));
+            }
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public ArrayList<Choice> getChoices() {
+            return choices;
+        }
+
+        public boolean containsSet(ArrayList<Choice> chosenAnswers) {
+            if (choices.size() != chosenAnswers.size()) {
+                return false;
+            }
+
+            for (Choice chosenAnswer : chosenAnswers) {
+                boolean found = false;
+                for (Choice choice : choices) {
+                    if (choice.getId() == chosenAnswer.getId()) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     private final ArrayList<Group> groups = new ArrayList<>();
 
     public GroupChoice(int id, Content content, Mode mode, ArrayList<Choice> choices) {
         super(id, content, mode, choices);
+    }
+
+    public static Group createGroup(char letter, String text, int[] intChoices) {
+        return new Group(letter, text, intChoices);
     }
 
     public void addGroups(ArrayList<Group> groups) {
@@ -25,23 +70,10 @@ public class GroupChoice extends Question {
     }
 
     public int calculateTotalScore(ArrayList<ArrayList<Choice>> groupsOfChosenAnswers) {
-        int score = 0;
-
-        for (ArrayList<Choice> chosenAnswers : groupsOfChosenAnswers) {
-            boolean correctPlacement = false;
-
-            for (Group group : groups) {
-                if (group.containsSet(chosenAnswers)) {
-                    correctPlacement = true;
-                    break;
-                }
-            }
-
-            if (correctPlacement) {
-                score++;
-            }
-        }
-        return score;
+        for (int i = 0; i < groupsOfChosenAnswers.size(); i++)
+            if (!groups.get(i).containsSet(groupsOfChosenAnswers.get(i)))
+                return 0;
+        return 1;
     }
 
     @Override
