41e1233fb6051dd414d33fae16acaafe7304b8b0
Daniel Mamani <danymamani1002@gmail.com>
2024-07-03 13:57:03 -0300

    #110 radio buttons are added to select necessary data that initializes the game

--- src/main/java/tp2/clases/screens/PlayersInputScreen.java
+++ src/main/java/tp2/clases/screens/PlayersInputScreen.java
@@ -15,6 +15,9 @@ import javafx.stage.Stage;
 import tp2.clases.Game;
 import tp2.clases.handlers.ConfirmButtonHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class PlayersInputScreen extends VBox {
@@ -24,8 +27,18 @@ public class PlayersInputScreen extends VBox {
     private TextField numberOfPlayersTextField;
     private TextField numberOfQuestionsTextField;
     private TextField numberOfPointsTextField;
+    private List<String> playerQuantityOptions = Arrays.stream("2, 3, 4, 5, 6".split(",")).toList();
+    private List<String> questionLimitOptions = Arrays.stream("5, 10, 15, 20, 25".split(",")).toList();
+    private List<String> scoreLimitOptions = Arrays.stream("10, 20, 30, 40, 50".split(",")).toList();
+    ToggleGroup toggleGroupPlayerQuantity;
+    ToggleGroup toggleGroupQuestionLimit;
+    ToggleGroup toggleGroupScoreLimit;
+    RadioButton selectedTogglePlayerQuantity;
+    RadioButton selectedToggleQuestionLimit;
+    RadioButton selectedToggleScoreLimit;
 
-    Stage stage;
+    private Game game;
+    private Stage stage;
     Button confirmButton;
 
     public PlayersInputScreen(Consumer<Integer> numberOfPlayersConsumer, Consumer<Integer> numberOfQuestionsConsumer, Consumer<Integer> numberOfPointsConsumer) {
@@ -72,6 +85,7 @@ public class PlayersInputScreen extends VBox {
         super(30);
 
         this.stage = primaryStage;
+        this.game = game;
 
         setAlignment(Pos.CENTER);
         setPadding(new Insets(20));
@@ -79,6 +93,9 @@ public class PlayersInputScreen extends VBox {
         Image image = new Image("file:white-background.jpg");
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         this.setBackground(new Background(backgroundImage));
+
+        displayPlayerOptionsToggleGroup();
+/*
 
         Label label = labelCreator("Ingrese la cantidad de jugadores:");
         playersCombo = new ComboBox<>(FXCollections.observableArrayList("2", "3", "4", "5", "6"));
@@ -91,15 +108,69 @@ public class PlayersInputScreen extends VBox {
         Label pointsLabel = labelCreator("Ingrese el limite de puntos:");
         pointsCombo = new ComboBox<>(FXCollections.observableArrayList("10", "20", "30", "40", "50"));
         addValidationListener(pointsCombo);
+*/
+
+        //getChildren().addAll(label, playersCombo, questionsLabel, questionsCombo, pointsLabel, pointsCombo, confirmButton);
+    }
+
+    private void displayPlayerOptionsToggleGroup() {
+        Label label = labelCreator("Seleccione la cantidad de jugadores:");
+        getChildren().add(label);
+        toggleGroupPlayerQuantity = new ToggleGroup();
+
+        for (String option: playerQuantityOptions) {
+            RadioButton radioButton = radioButtonCreator(option, toggleGroupPlayerQuantity);
+            radioButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue.equals("")) {
+                    selectedTogglePlayerQuantity = (RadioButton) toggleGroupPlayerQuantity.getSelectedToggle();
+                    getChildren().clear();
+                    displayQuestionLimitOptionsToggleGroup();
+                }
+            });
+            getChildren().add(radioButton);
+        }
+    }
+
+    private void displayQuestionLimitOptionsToggleGroup() {
+        Label label = labelCreator("Seleccione el limite de preguntas:");
+        getChildren().add(label);
+        toggleGroupQuestionLimit = new ToggleGroup();
+
+        for (String option : questionLimitOptions) {
+            RadioButton radioButton = radioButtonCreator(option, toggleGroupQuestionLimit);
+            radioButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue.equals("")) {
+                    selectedToggleQuestionLimit = (RadioButton) toggleGroupQuestionLimit.getSelectedToggle();
+                    getChildren().clear();
+                    displayScoreLimitOptionsToggleGroup();
+                }
+            });
+            getChildren().add(radioButton);
+        }
+    }
+
+    private void displayScoreLimitOptionsToggleGroup() {
+        Label label = labelCreator("Seleccione el limite de puntos:");
+        getChildren().add(label);
+        toggleGroupScoreLimit = new ToggleGroup();
+
+        for (String option: scoreLimitOptions) {
+            RadioButton radioButton = radioButtonCreator(option, toggleGroupScoreLimit);
+            radioButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                if (!newValue.equals("")) {
+                    selectedToggleScoreLimit = (RadioButton) toggleGroupScoreLimit.getSelectedToggle();
+                }
+            });
+            getChildren().add(radioButton);
+        }
 
         confirmButton = new Button();
         confirmButton.setText("Confirmar");
         confirmButton.setStyle("-fx-font-size: 14px; -fx-background-color: #090971; -fx-text-fill: white;");
-        ConfirmButtonHandler confirmButtonHandler = new ConfirmButtonHandler(game, this, primaryStage);
+        ConfirmButtonHandler confirmButtonHandler = new ConfirmButtonHandler(game, this, stage);
         confirmButton.setOnAction(confirmButtonHandler);
-        confirmButton.setDisable(true);
-
-        getChildren().addAll(label, playersCombo, questionsLabel, questionsCombo, pointsLabel, pointsCombo, confirmButton);
+        confirmButton.setDisable(false);
+        getChildren().add(confirmButton);
     }
 
     private void addValidationListener(ComboBox<String> comboBox) {
@@ -114,21 +185,33 @@ public class PlayersInputScreen extends VBox {
     }
 
     public int getNumberOfPlayersInput() {
-        return Integer.parseInt(playersCombo.getValue());
+        return Integer.parseInt(selectedTogglePlayerQuantity.getText());
+        //return Integer.parseInt(playersCombo.getValue());
     }
 
     public int getQuestionLimitInput() {
-        return Integer.parseInt(questionsCombo.getValue());
+        return Integer.parseInt(selectedToggleQuestionLimit.getText());
+        //return Integer.parseInt(questionsCombo.getValue());
     }
 
     public int getPointLimitInput() {
-        return Integer.parseInt(pointsCombo.getValue());
+        return Integer.parseInt(selectedToggleScoreLimit.getText());
+        //return Integer.parseInt(pointsCombo.getValue());
     }
 
     private Label labelCreator(String text) {
         Label label = new Label(text);
-        label.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-text-fill: #333;");
+        label.setStyle("-fx-font-size: 30px; -fx-font-weight: bold; -fx-text-fill: #333;");
         return label;
+    }
+
+    private RadioButton radioButtonCreator(String text, ToggleGroup toggleGroup) {
+        RadioButton radioButton = new RadioButton(text);
+        radioButton.setToggleGroup(toggleGroup);
+        radioButton.setSelected(false);
+        radioButton.setStyle("-fx-background-radius: 15px; -fx-padding: 8px; -fx-border-width: 1px; -fx-font-size: 20px; -fx-stroke-width: 2px;");
+
+        return radioButton;
     }
 
     private TextField textFieldCreator(String promptText) {
